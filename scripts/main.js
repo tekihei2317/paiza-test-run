aa7e6f85f689f3aa8395477de644860b87264148
tekihei2317 <tekihei2317@gmail.com>
2021-02-10 22:48:07 +0900

    fix: 言語選択後ペースト出来るように修正
    changeイベントが発生していなかったため

--- scripts/main.js
+++ scripts/main.js
@@ -15,13 +15,15 @@
     });
   }
 
-  function setDefaultLanguage() {
-    // TODO: 再チャレンジ画面では言語を変更しないようにする
+  async function setDefaultLanguage() {
     const languageSelect = document.getElementById("language_id");
     if (languageSelect === null) return;
 
     const defaultLanguageId = localStorage.getItem("default_language_id");
     languageSelect.value = defaultLanguageId;
+
+    // changeイベントを発火させる(参考: https://ja.stackoverflow.com/questions/55862/%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%81%A7%E8%A1%A8%E7%A4%BA%E3%81%97%E3%81%A6%E3%81%84%E3%82%8Bweb%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AEselect%E8%A6%81%E7%B4%A0%E3%81%AEonchange%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E3%82%92%E7%99%BA%E7%81%AB%E3%81%95%E3%81%9B%E3%82%8B%E6%96%B9%E6%B3%95)
+    languageSelect.dispatchEvent(new Event("change"));
   }
 
   async function selectLanguage(languageId) {
